9a2a2c3bc061fb07a3dd9e1829430f708d510f65
lamkovod <alexvodolazko@gmail.com>
2017-08-25 00:42:29 +0300

    - first test with arquillium - set up th environment

--- src/test/java/pages/MainPage.java
+++ src/test/java/pages/MainPage.java
@@ -1,4 +1,12 @@
 package pages;
 
+import java.io.PrintStream;
+
 public class MainPage {
+    public void test1(PrintStream to, String name){
+        to.println(createmem(name));
+    }
+    public String createmem(String name){
+        return "Hello, " + name + "!";
+    }
 }
--- src/test/java/tests/LoginTestSet.java
+++ src/test/java/tests/LoginTestSet.java
@@ -1,4 +1,36 @@
 package tests;
 
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import pages.MainPage;
+import sun.tools.jar.resources.jar;
+
+import javax.inject.Inject;
+import java.io.PrintStream;
+
+@RunWith(Arquillian.class)
 public class LoginTestSet {
+
+    @Deployment
+    public static JavaArchive createDeployment(){
+        return ShrinkWrap.create(JavaArchive.class).
+                addClass(MainPage.class)
+                .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
+    }
+
+    @Inject
+    private MainPage mem;
+
+    @Test
+    public void first_test(){
+        Assert.assertEquals("Hello, Friend!",
+                mem.createmem("Friend"));
+        mem.test1(System.out, "Friend");
+    }
 }
